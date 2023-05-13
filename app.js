d5c835691b682cb5b59e39ce844c25543a75575c
MuneimAsif <muneimasif2004@gmail.com>
2023-05-12 19:57:30 -0700

    Another Practise Repo

---
+++ app.js
@@ -0,0 +1,114 @@
+// var cities = ['karachi','lahore','islamabad','multan']
+// var cityName = prompt('Enter City Name');
+// for(var i = 0; i<=3;  i++ ){
+//     if(cityName == cities[i]){
+//         console.log("match")
+//         break;
+// }else{}
+// }
+
+
+// var a = ["karachi","lahore"]
+// var b = a.indexOf("lahore")
+// console.log(b)
+
+// var cityName = prompt("Enter City Name")
+// var cities = ["karachi","lahore","islamabad","multan","quetta"]
+// cityName = cityName.toLowerCase()
+// for(var i = 0; i < cities.length; i++){
+//     if(cityName == cities[i]){
+//          console.log("It's one of the cleanest cities")
+//          break;
+//     }else{}
+    
+// }
+
+// var a = "Muneim"
+// var b = "Muneim"
+// var c = "Muneim"
+// var d = "Muneim"
+// var e = "Muneim"
+// var f = "Muneim"
+
+// var g = a.slice(0,1)
+// var h = b.slice(1,2)
+// var i = c.slice(2,3)
+// var j = d.slice(3,4)
+// var k = e.slice(4,5)
+// var l = f.slice(5,6)
+ 
+// // g = g.charAt(g)
+// // h = h.charAt(h)
+// // i = i.charAt(i)
+// // j = j.charAt(j)
+// // k = k.charAt(k)
+// // l = l.charAt(l)
+
+// console.log(g+h+i+j+k+l)
+
+// var a = "Biryani"
+// var b = a.replace(/Biryani/g,"S")
+// console.log(b)
+
+
+// var a = 'Faizan-e'
+// a = a.replace('e','e-Madina')
+// console.log(a)
+
+// ==================DICE===============
+// var a = Math.random()
+// var b = Math.ceil(a*6)
+// console.log(b)
+
+// var a = Math.random()
+// var b = Math.ceil(a*6)
+// console.log(b)
+
+// ===================TOSS==============
+
+// var a = Math.random();
+// var dice = Math.ceil(a*6)
+// if(dice == 1 || dice == 3 || dice == 5){
+//     document.write("heads")
+// }so
+// else{
+//     document.write("tails")
+// }
+
+
+
+// To convert string into numbers
+// var a = 123.23
+// a = +a
+// a = Number(a)
+// a = parseInt(a)
+// a = parseFloat(a)
+// To convert number into string
+// a = a.toString();
+
+// var date = new Date();
+// var days = ["sun","mon","tue","wed","thus","fri","sat"]
+// consolelog("Date ===> ",date.getDate())
+// console.log("Day ===> " ,days[date.getDay()])
+// console.log("Year ===>",date.getFullYear())
+// console.log("Hours",date.getHours())
+// console.log("Miliseconds ===>",date.getMilliseconds())
+// console.log("Minutes ===>",date.getMinutes())
+// console.log("Month ===>",date.getMonth())
+// console.log("Seconds ===>",date.getSeconds())
+// console.log("Time ===>",date.getTime())
+
+// var date = new Date("01-15-2000");
+
+// var b = 21321144324343
+// b = b/(1000*60*60*24*365
+
+// ================== Age Calculator=====================// var date = new Date();
+// var user = prompt("Enter DOB")
+// var dob = new Date(user)
+// console.log(date.getTime())
+// console.log(dob.getTime())
+// var a = date - dob
+// var b = a/(1000*60*60*24*365)
+// var c = Math.floor(b);
+// console.log(c)
